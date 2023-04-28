ae493e0e3f27e11482dd65045154939a124b2d3a
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2023-04-28 16:23:51 +0200

    feat: New i3 and fish config

---
+++ .config/fish/fuchsia_frustration.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Colorscheme: Fuchsia Frustration
+set -U fish_color_normal 450731
+set -U fish_color_command ff65d4
+set -U fish_color_quote f3ffff
+set -U fish_color_redirection f3ffff
+set -U fish_color_end ff65d4
+set -U fish_color_error d54e53
+set -U fish_color_param 7aa6da
+set -U fish_color_comment f3ffff
+set -U fish_color_match --background=919bfd
+set -U fish_color_selection f3ffff --bold --background=131021
+set -U fish_color_search_match 919bfd --background=131021
+set -U fish_color_history_current --bold
+set -U fish_color_operator ff65d4
+set -U fish_color_escape ff65d4
+set -U fish_color_cwd 131021
+set -U fish_color_cwd_root d54e53
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 969896
+set -U fish_color_user 919bfd
+set -U fish_color_host ff65d4
+set -U fish_color_cancel --reverse
+set -U fish_pager_color_background 
+set -U fish_pager_color_prefix normal --bold --underline
+set -U fish_pager_color_progress f3ffff --background=919bfd
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D
+set -U fish_pager_color_selected_background --background=131021
+set -U fish_pager_color_selected_prefix 
+set -U fish_pager_color_selected_completion 
+set -U fish_pager_color_selected_description 
+set -U fish_color_host_remote 
+set -U fish_color_option 
+set -U fish_pager_color_secondary_completion 
+set -U fish_pager_color_secondary_background 
+set -U fish_pager_color_secondary_prefix 
+set -U fish_pager_color_secondary_description 
+set -U fish_color_keyword 
